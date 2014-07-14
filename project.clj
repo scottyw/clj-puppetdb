1bea4c323ecd269cb9d5bfa0c2a12dd96b7578b7
Justin Holguin <justin.holguin@puppetlabs.com>
2014-07-13 21:23:12 -0700

    Added some experimental schema-checking

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [puppetlabs/http-client "0.1.7"]
+                 [prismatic/schema "0.2.4"]
                  [cheshire "5.3.1"]])
