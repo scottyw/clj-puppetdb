71c687e99524a4e2edd94d3dc7f6c97e098192e0
Justin Holguin <justin.h.holguin@gmail.com>
2014-07-18 19:39:54 -0700

    WIP: query macro :-(

---
+++ src/clj_puppetdb/query.clj
@@ -0,0 +1,65 @@
+(ns clj-puppetdb.query
+  (:require [clojure.walk :refer [postwalk]]
+            [cheshire.core :as json]))
+
+(def ops
+  {'= "="
+   '> ">"
+   '< "<"
+   '>= ">="
+   '<= "<="
+;   '~ "~"
+   'null? "null?"
+   'nil? "null?"
+   'or "or"
+   'and "and"})
+
+(defn operator?
+  [x]
+  (contains? ops x))
+
+(defn- name-of
+  "Only used as a helper for query-walk. Gets the name of 
+  a named symbol. Without this, using ~ in a query has
+  weird behavior."
+  [x]
+  (case (name x)
+    "unquote" "~"
+    (name x)))
+
+(defn query-walk
+  "This does most of the hard work for generating a query.
+  It's intended to be applied recursively to each and every
+  element of an expression to ensure that the final result
+  can be converted straightforwardly into JSON. Just about
+  anything that can be turned into a string will be, but
+  booleans and numbers pass through unchanged."
+  [x]
+  (cond
+   (operator? x) (ops x)
+   (list? x) (vec x)
+   (instance? java.util.regex.Pattern x) (str x)
+   (instance? clojure.lang.Named x) (name x)
+   (string? x) x
+   (= \~ (char x)) "~"
+   :else x))
+
+(defn query'
+  "The function behind the query macro. Takes a quoted
+  S-expression and turns it into a JSON array. Call this
+  directly if you can't use the macro."
+  [q]
+  (json/encode (postwalk query-walk q)))
+
+(defmacro query
+  "Takes an unquoted S-expression representing a PuppetDB
+  query and returns a JSON array suitable for sending to
+  PuppetDB."
+  [q]
+  `(json/encode (postwalk query-walk ~q)))
+
+(defmacro qv
+  "EXPERIMENTAL: return a Query Vector that can be turned into
+  a JSON query later. This might let the expression include vars."
+  [q]
+  (postwalk query-walk q))
