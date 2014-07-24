4872b4fe26e7702b483dbe953e6b2c61f62dd3cb
Justin Holguin <justin.h.holguin@gmail.com>
2014-07-23 20:36:30 -0700

    WIP: Results are truly lazy

--- src/clj_puppetdb/paging.clj
+++ src/clj_puppetdb/paging.clj
@@ -4,22 +4,23 @@
             [cheshire.core :as json]))
 
 ;; TODO:
-;; - The first set of results gets queried immediately.
-;;   That's not that lazy.
+;; - The way that results are refreshed causes an extra query
+;;   even when we _know_ that there will not be any further results
+;;   (because a query has already failed).
 ;; - This namespace is positively RIPE for refactoring.
 
 (defn- falling-behind?
   "Returns true if the results map contains <= 1 result."
   [results]
-  (>= 1
+  (zero?
       (count (:body results))))
 
 (defn- refresh
   "Given a results map from a paging query, request and return the 
   next set of results."
   [{:keys [limit offset query]}]
-  (let [new-offset (+ limit offset)
-        new-body (query new-offset limit)]
+  (let [new-body (query offset limit)
+        new-offset (+ limit offset)]
     (println "Querying! Offset:" new-offset)
     {:body new-body
      :limit limit
@@ -32,15 +33,16 @@
   [results]
   (if (falling-behind? results)
     (refresh results)
-    (update-in results [:body] rest)))
+    results))
 
 (defn- lazy-page
   [results]
   (lazy-seq
-   (when (seq (:body results))
-      (cons
-       (first (:body results))
-       (lazy-page (ensure-refreshed results))))))
+    (let [refreshed (ensure-refreshed results)]
+      (when (seq (:body refreshed))
+        (cons
+          (first (:body refreshed))
+          (lazy-page (update-in refreshed [:body] rest)))))))
 
 (defn- lazify-query
   "Returns a map containing initial results with enough contextual data
@@ -51,7 +53,7 @@
                       (GET conn path (assoc json-params :offset offset)))
            limit (:limit params)
            offset (get params :offset 0)]
-       {:body (query-fn offset limit)
+       {:body nil
         :limit limit
         :offset offset
         :query query-fn}))
@@ -79,7 +81,8 @@
 (comment
 
 (require '[clojure.java.io :as io]
-         '[clojure.pprint :refer [pprint]])
+         '[clojure.pprint :refer [pprint]]
+         '[clj-puppetdb.core :as pdb])
   
 (def conn
   (connect "https://puppetdb:8081"
@@ -90,5 +93,16 @@
 (def lazy-facts
   (lazy-query conn "/v4/facts"
               {:limit 100 :offset 0 :order-by [{:field :value :order "asc"}]}))
+
+(def lazy-fact-map
+  (lazify-query conn "/v4/facts"
+                {:limit 100 :offset 0 :order-by [{:field :name :order "asc"}]}))
+
+
+(def lazy-fact-results
+                       (set (take 300 (lazy-page lazy-fact-map))))
+
+(def facts
+  (set (pdb/query conn "/v4/facts")))
 
 )
