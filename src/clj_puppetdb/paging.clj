49f8708a7331ad7b5015548c146ac355b0bc44e1
Justin Holguin <justin.h.holguin@gmail.com>
2014-07-24 18:58:17 -0700

    WIP: Added schema checking

--- src/clj_puppetdb/paging.clj
+++ src/clj_puppetdb/paging.clj
@@ -1,16 +1,14 @@
 (ns clj-puppetdb.paging
   (:require [clj-puppetdb.core :refer [connect GET]]
+            [clj-puppetdb.schema :refer [PagingParams Connection]]
             [clj-puppetdb.query :as q]
-            [cheshire.core :as json]))
-
-;; TODO:
-;; - There should be some schema checking in here
-
+            [cheshire.core :as json]
+            [schema.core :as s]))
 
 (defn- refresh
   "Given a results map from a paging query, request and return the 
   map with the next set of results, or return nil if there are no
-  more results."
+  more results. Never returns a map with an empty :body."
   [{:keys [limit offset query]}]
   (when-let [new-body (query offset)]
     {:body new-body
@@ -23,7 +21,7 @@
 
 (defn- ensure-refreshed
   "Given a results map, refreshes it if necessary. Otherwise, return
-  the results unchanged. Never returns a map with an empty body; 
+  the results unchanged. Never returns a map with an empty :body; 
   The return value will be nil if the results have been exhausted."
   [results]
   (if (empty? (:body results))
@@ -43,21 +41,21 @@
 (defn- lazify-query
   "Returns a map containing initial results with enough contextual data
   to request the next set of results."
-  ([conn path params]
+  ([client path params]
      (let [json-params (update-in params [:order-by] json/encode)
            query-fn (fn [offset]
-                      (GET conn path (assoc json-params :offset offset)))
+                      (GET client path (assoc json-params :offset offset)))
            limit (:limit params)
            offset (get params :offset 0)]
        {:body nil
         :limit limit
         :offset offset
         :query query-fn}))
-  ([conn path query-vec params]
+  ([client path query-vec params]
      (let [params-with-query (assoc params :query (q/query->json query-vec))]
-       (lazify-query conn path params-with-query))))
+       (lazify-query client path params-with-query))))
 
-(defn lazy-query
+(s/defn ^:always-validate lazy-query
   "Return a lazy sequence of results from the given query. Unlike the regular
   `query` function, `lazy-query` uses paging to fetch results gradually as they
   are consumed.
@@ -67,11 +65,11 @@
   - :offset (optional: the index of the first result to return, default 0)
   - :order-by (a vector of maps, each specifying a :field and an :order key)
   For example: `{:limit 100 :offset 0 :order-by [{:field \"value\" :order \"asc\"}]}`"
-  ([connection path params]
-     (-> (lazify-query connection path params)
+  ([client path params :- PagingParams]
+     (-> (lazify-query client path params)
          lazy-page))
-  ([connection path query-vec params]
-     (-> (lazify-query connection path query-vec params)
+  ([client path query-vec params :- PagingParams]
+     (-> (lazify-query client path query-vec params)
          lazy-page)))
 
 (comment
@@ -80,14 +78,14 @@
          '[clojure.pprint :refer [pprint]]
          '[clj-puppetdb.core :as pdb])
   
-(def conn
+(def client
   (connect "https://puppetdb:8081"
            {:ssl-ca-cert (io/file "/Users/justin/certs/ca.pem")
             :ssl-cert (io/file "/Users/justin/certs/cert.pem")
             :ssl-key (io/file "/Users/justin/certs/private.pem")}))
 
 (def lazy-facts
-  (lazy-query conn "/v4/facts"
+  (lazy-query client "/v4/facts"
               {:limit 100 :offset 0 :order-by [{:field :value :order "asc"}]}))
 
 )
