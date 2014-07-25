18b8f2a016cc1f16bd179597a7ab82571d8e86d4
Justin Holguin <justin.h.holguin@gmail.com>
2014-07-24 20:17:17 -0700

    Add licensing information and set version to 0.1.0

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject clj-puppetdb "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject clj-puppetdb "0.1.0"
+  :description "A Clojure client for the PuppetDB REST API"
   :url "https://github.com/holguinj/clj-puppetdb"
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [puppetlabs/http-client "0.1.7"]
                  [prismatic/schema "0.2.4"]
