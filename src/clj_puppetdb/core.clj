abc1b42601dd7fb9f65e01b01a142859010bd064
Scott Walker <scott.walker@puppetlabs.com>
2015-02-10 16:36:38 +0000

    Add a four-arg version of query to support retrieving a single page of results in an eager (rather than lazy) manner

--- src/clj_puppetdb/core.clj
+++ src/clj_puppetdb/core.clj
@@ -1,5 +1,6 @@
 (ns clj-puppetdb.core
-  (:require [clj-puppetdb.http :refer [GET] :as http]
+  (:require [cheshire.core :as json]
+            [clj-puppetdb.http :refer [GET] :as http]
             [clj-puppetdb.paging :as paging]
             [clj-puppetdb.query :as q]
             [clj-puppetdb.schema :refer [Client]]
@@ -23,24 +24,41 @@
   
   The path argument should be a valid endpoint, e.g. \"/v4/nodes\".
 
-  The query-vec argument should be a vector representing an API query,
-  e.g. [:= [:fact \"operatingsystem\"] \"Linux\"]"
+  The query-vec argument is optional, and should be a vector representing an API query,
+  e.g. [:= [:fact \"operatingsystem\"] \"Linux\"]
+
+  The `params` map is optional, and should contain the following keys:
+  - :limit (the number of results to request)
+  - :offset (optional: the index of the first result to return, default 0)
+  - :order-by (a vector of maps, each specifying a :field and an :order key)
+  For example: `{:limit 100 :offset 0 :order-by [{:field \"value\" :order \"asc\"}]}`"
+  ([client path]
+    (query client path nil nil))
   ([client path query-vec]
-     (let [query-string (q/query->json query-vec)]
-       (GET client path {:query query-string})))
-  ([client path] (GET client path)))
+    (query client path query-vec nil))
+  ([client path query-vec paging-params]
+    (let [params (-> nil
+                     (merge
+                       (when query-vec
+                         {:query (q/query->json query-vec)}))
+                     (merge
+                       (when paging-params
+                         (update-in paging-params [:order-by] json/encode))))]
+      (if params
+        (GET client path params)
+        (GET client path)))))
 
 (defn lazy-query
   "Return a lazy sequence of results from the given query. Unlike the regular
   `query` function, `lazy-query` uses paging to fetch results gradually as they
   are consumed.
-  
+
   The `params` map is required, and should contain the following keys:
   - :limit (the number of results to request)
   - :offset (optional: the index of the first result to return, default 0)
   - :order-by (a vector of maps, each specifying a :field and an :order key)
   For example: `{:limit 100 :offset 0 :order-by [{:field \"value\" :order \"asc\"}]}`"
   ([client path params]
-     (paging/lazy-query client path params))
+    (paging/lazy-query client path params))
   ([client path query-vec params]
-     (paging/lazy-query client path query-vec params)))
+    (paging/lazy-query client path query-vec params)))
