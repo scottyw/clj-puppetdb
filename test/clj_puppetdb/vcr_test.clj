8f159905df8ff212fb70a22f3426b977bbe80254
Scott Walker <scott.walker@puppetlabs.com>
2015-03-10 12:15:04 +0000

    Update tests to verify different query params are recorded separately that that query param reordering does not affect recording.

--- test/clj_puppetdb/vcr_test.clj
+++ test/clj_puppetdb/vcr_test.clj
@@ -2,21 +2,21 @@
   (:require [clojure.test :refer :all]
             [clj-puppetdb.core :refer :all]
             [clj-puppetdb.http :refer :all]
+            [clj-puppetdb.query :as q]
             [puppetlabs.http.client.sync :as http]
             [me.raynes.fs :as fs]))
 
-(def mock-http-response {:opts                  {:persistent      false
-                                                 :as              :text
-                                                 :decompress-body true
-                                                 :body            nil
-                                                 :headers         {}
-                                                 :method          :get
-                                                 :url             "http://pe:8080/v4/nodes"}
-                         :orig-content-encoding "gzip"
-                         :status                200
-                         :headers               {"server" "test"}
-                         :content-type          {:mime-type "application/json"}
-                         :body                  " {\"test\": \"foo\"} "})
+(def mock-http-response-template {:opts                  {:persistent      false
+                                                          :as              :text
+                                                          :decompress-body true
+                                                          :body            nil
+                                                          :headers         {}
+                                                          :method          :get
+                                                          :url             "http://pe:8080/v4/nodes"}
+                                  :orig-content-encoding "gzip"
+                                  :status                200
+                                  :headers               {"server" "test"}
+                                  :content-type          {:mime-type "application/json"}})
 
 (deftest vcr-test
   (testing "VCR recording and replay"
@@ -29,26 +29,60 @@
             (with-redefs [http/get
                           (fn [& rest]
                             ; Return mock data
-                            mock-http-response)]
+                            (assoc mock-http-response-template :body " {\"test\": \"all-nodes\"} "))]
               ; Real response, should be recorded
-              (is (= [{:test "foo"} {"server" "test"}] (GET conn "/v4/nodes"))))
-            (is (not-empty (fs/list-dir vcr-dir))))
+              (is (= [{:test "all-nodes"} {"server" "test"}] (GET conn "/v4/nodes"))))
+            (with-redefs [http/get
+                          (fn [& rest]
+                            ; Return mock data
+                            (assoc mock-http-response-template :body " {\"test\": \"some-nodes\"} "))]
+              ; Real response, should be recorded
+              (is (= [{:test "some-nodes"} {"server" "test"}] (GET conn "/v4/nodes"
+                                                                   {:query    (q/query->json [:= :certname "test"])
+                                                                    :order-by "something"}))))
+            ; There should be 2 recordings
+            (is (= 2 (count (fs/list-dir vcr-dir)))))
           (testing "and a recording already exists"
-            (is (= [{:test "foo"} {"server" "test"}] (GET conn "/v4/nodes"))))
+            (is (= [{:test "all-nodes"} {"server" "test"}] (GET conn "/v4/nodes")))
+            (is (= [{:test "some-nodes"} {"server" "test"}] (GET conn "/v4/nodes"
+                                                                 {:query    (q/query->json [:= :certname "test"])
+                                                                  :order-by "something"})))
+            (is (= [{:test "some-nodes"} {"server" "test"}] (GET conn "/v4/nodes"
+                                                                 {:order-by "something"
+                                                                  :query    (q/query->json [:= :certname "test"])}))))
           (testing "and a recording already exists and the real endpoint has changed"
             (with-redefs [http/get
                           (fn [& rest]
                             ; Return mock data but modified
-                            (assoc mock-http-response :body " {\"test\": \"different-body-this-time\"} "))]
-              ; VCR enabled so we expect to see the original body
-              (is (= [{:test "foo"} {"server" "test"}] (GET conn "/v4/nodes")))))))
-      (testing "when VCR is not enabled but a recording exists"
-        (let [conn (connect "http://localhost:8080")]
-          (is (nil? (:vcr-dir conn)))
-          (with-redefs [http/get
-                        (fn [& rest]
-                          ; Return mock data but modified
-                          (assoc mock-http-response :body " {\"test\": \"different-body-this-time\"} "))]
-            ; VCR disabled so we expect to see the different body
-            (is (= [{:test "different-body-this-time"} {"server" "test"}] (GET conn "/v4/nodes"))))))
-      (fs/delete-dir vcr-dir))))
+                            (assoc mock-http-response-template :body " {\"test\": \"different-body-this-time\"} "))]
+              ; VCR enabled so we expect to see the original bodies
+              (is (= [{:test "all-nodes"} {"server" "test"}] (GET conn "/v4/nodes")))
+              (is (= [{:test "some-nodes"} {"server" "test"}] (GET conn "/v4/nodes"
+                                                                   {:order-by "something"
+                                                                    :query    (q/query->json [:= :certname "test"])})))
+              (is (= [{:test "some-nodes"} {"server" "test"}] (GET conn "/v4/nodes"
+                                                                   {:query    (q/query->json [:= :certname "test"])
+                                                                    :order-by "something"}))))))
+        (testing "when VCR is not enabled but a recording exists"
+          (let [conn (connect "http://localhost:8080")]
+            (is (nil? (:vcr-dir conn)))
+            (with-redefs [http/get
+                          (fn [& rest]
+                            ; Return mock data
+                            (assoc mock-http-response-template :body " {\"test\": \"all-nodes-changed\"} "))]
+              ; Real response, should not be recorded
+              (is (= [{:test "all-nodes-changed"} {"server" "test"}] (GET conn "/v4/nodes"))))
+            (with-redefs [http/get
+                          (fn [& rest]
+                            ; Return mock data
+                            (assoc mock-http-response-template :body " {\"test\": \"some-nodes-changed\"} "))]
+              ; Real response, should not be recorded
+              (is (= [{:test "some-nodes-changed"} {"server" "test"}] (GET conn "/v4/nodes"
+                                                                           {:order-by "something"
+                                                                            :query    (q/query->json [:= :certname "test"])})))
+              (is (= [{:test "some-nodes-changed"} {"server" "test"}] (GET conn "/v4/nodes"
+                                                                           {:order-by "something"
+                                                                            :query    (q/query->json [:= :certname "test"])}))))
+            ; There should still be just 2 recordings
+            (is (= 2 (count (fs/list-dir vcr-dir)))))
+          (fs/delete-dir vcr-dir))))))
