093cc171e19014a0182843d766443a3fc13cbce5
Scott Walker <scott.walker@puppetlabs.com>
2015-03-16 15:44:38 +0000

    Fix broken param handling

--- src/clj_puppetdb/core.clj
+++ src/clj_puppetdb/core.clj
@@ -19,15 +19,23 @@
   ([^String host] (connect host {}))
   ([^String host opts] (http/make-client host opts)))
 
-(defn- encode-order-by
+(defn- encode-order-by-using-keyword
   "order-by params contain nested maps and if the VCR running we want to enforce a specific ordering to give us URL stability"
-  [client params]
-  (update-in params [:order-by]
-    (if (get-in client [:opts :vcr-dir])
-      ; Stability of URL is important
-      (fn [order-by] json/encode (map (fn [map] into (sorted-map) map) order-by))
-      ; Stability of URL doesn't matter
-      json/encode)))
+  [params client keyword]
+  (if (contains? params keyword)
+    (update-in params [keyword]
+      (if (get-in client [:opts :vcr-dir])
+        ; Stability of URL is important
+        (fn [order-by] json/encode (map (fn [map] into (sorted-map) map) order-by))
+        ; Stability of URL doesn't matter
+        json/encode))
+    params))
+
+(defn- encode-order-by
+  [params client]
+  (-> params
+    (encode-order-by-using-keyword client :order-by)        ; v3 and earlier
+    (encode-order-by-using-keyword client :order_by)))      ; v4
 
 (defn query-with-metadata
   "Use the given PuppetDB client to query the server.
@@ -50,15 +58,14 @@
   ([client path params]
     (query-with-metadata client path nil params))
   ([client path query-vec params]
-    (let [merged-params (merge {}
-                               (if query-vec
-                                 {:query (q/query->json query-vec)})
-                               (if (contains? params :order-by)
-                                 (encode-order-by client params)))
-          [body headers] (GET client path merged-params)
-          total (get headers "x-records")
-          metadata (if total {:total total})]
-      [body metadata])))
+   (let [merged-params (merge params
+                         (if query-vec
+                           {:query (q/query->json query-vec)}))
+         merged-params (encode-order-by merged-params client)
+         [body headers] (GET client path merged-params)
+         total         (get headers "x-records")
+         metadata      (if total {:total total})]
+     [body metadata])))
 
 (defn query
   "Use the given PuppetDB client to query the server.
