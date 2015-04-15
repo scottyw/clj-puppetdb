d8fc0f1b6d6156ea606ca3c25cde7eab766a952e
Jenkins CI <ci@puppetlabs.com>
2015-04-15 08:33:33 -0700

    Version 0.2.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject puppetlabs/clj-puppetdb "0.2.0"
+(defproject puppetlabs/clj-puppetdb "0.2.1-SNAPSHOT"
   :description "A Clojure client for the PuppetDB REST API"
   :url "https://github.com/puppetlabs/clj-puppetdb"
   :license {:name "Apache License, Version 2.0"
